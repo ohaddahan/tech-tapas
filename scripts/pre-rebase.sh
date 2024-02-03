af2f61407374b71aa553c1dbfd61c156989dc6e4
ohaddahan <ohaddahan@gmail.com>
2024-02-03 17:52:08 +0200

    blabl

--- scripts/pre-rebase.sh
+++ scripts/pre-rebase.sh
@@ -3,13 +3,7 @@ set -x
 source ./scripts/helpers.sh
 function git_branch_name()
 {
-  local branch=$(git symbolic-ref HEAD 2> /dev/null | awk 'BEGIN{FS="/"} {print $NF}')
-  if [[ $branch == "" ]];
-  then
-    :
-  else
-    echo '- ('$branch')'
-  fi
+  echo "$(git symbolic-ref HEAD 2> /dev/null | awk 'BEGIN{FS="/"} {print $NF}')"
 }
 
 ensure current_branch=$(git_branch_name)
