db95d3180937df763e31c078871fbb26bcae796c
ohaddahan <ohaddahan@gmail.com>
2024-02-03 17:42:25 +0200

    yay

---
+++ scripts/pre-rebase.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -x
+source ./scripts/helpers.sh
+function git_branch_name()
+{
+  local branch=$(git symbolic-ref HEAD 2> /dev/null | awk 'BEGIN{FS="/"} {print $NF}')
+  if [[ $branch == "" ]];
+  then
+    :
+  else
+    echo '- ('$branch')'
+  fi
+}
+
+ensure current_branch=$(git_branch_name)
+ensure git checkout -b "${current_branch}_backup"
+ensure git push
+ensure git checkout "${current_branch}"
