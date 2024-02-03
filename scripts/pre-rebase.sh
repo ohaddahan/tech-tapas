49ac33053bfae4d983926f09a7cc838312e881c0
ohaddahan <ohaddahan@gmail.com>
2024-02-03 17:52:43 +0200

    s

--- scripts/pre-rebase.sh
+++ scripts/pre-rebase.sh
@@ -6,7 +6,7 @@ function git_branch_name()
   echo "$(git symbolic-ref HEAD 2> /dev/null | awk 'BEGIN{FS="/"} {print $NF}')"
 }
 
-ensure current_branch=$(git_branch_name)
+ensure current_branch="$(git_branch_name)"
 ensure git checkout -b "${current_branch}_backup"
 ensure git push
 ensure git checkout "${current_branch}"
